246f0afef03b35b1af6fdf8ffe932e519d03d05f
Abdul Hannan Tahir <a.hannantahir@gmail.com>
2019-08-07 09:19:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,5 @@
 function basicTeenager(age) {
+<<<<<<< HEAD
 if (age>=13 && age<=19) {
 return "You are a teenager!";
 }
@@ -19,10 +20,32 @@ function ageChecker(age) {
   return "You are a kid" }
   else {
     return "You are a grownup"
+=======
+basicTeenager(13);
+if (age>12 && age<20)
+"You are a teenager!";
+}
+
+function teenager(age) {
+  if (age>=13 && age<=19)
+  "You are a teenager!"
+  else ()
+  "You are not a teenager!"
+}
+
+function ageChecker(age) {
+  if (age>=13 && age<=19)
+  "You are a teenager!"
+  else if (age<13)
+  "You are a kid!"
+  else {
+    "You are a grownup"
+>>>>>>> 2af8c02667084200e19d958350b9284935d924e5
   }
 }
 
 function ternaryTeenager(age) {
+<<<<<<< HEAD
 return age>=13 && age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
@@ -40,4 +63,16 @@ function switchAge(age) {
       default:
       return "You have an age"
     }
+=======
+age>=13 && age<=19 ? "You are a teenager!" : "You are not a teenager!"
+}
+
+function switchAge(age) {
+  witch(age) {
+    case 13, 14, 15, 16, 17, 18, 19:
+      console.log("You are a teenager");
+      break;
+      default:
+      console.log("You have an age")
+>>>>>>> 2af8c02667084200e19d958350b9284935d924e5
 }
